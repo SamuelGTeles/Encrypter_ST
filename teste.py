4ef78c23c5c656a512fcad678a419ce02d0e8e66
SamuelGTeles <samuelteles789@gmail.com>
2026-06-03 12:28:59 -0300

    ...

--- teste.py
+++ teste.py
@@ -1,1 +1,8 @@
-print("olá")
+nums = input("Quantos números você irá utilizar na operacão? 1/2/3/4 :")
+
+num1 = input(int("Digite o primeiro número :"))
+num2 = input(int("Digite o segundo número :"))
+num3 = input(int("Digite o terceiro número :"))
+num4 = input(int("Digite o quarto número :"))
+
+if
